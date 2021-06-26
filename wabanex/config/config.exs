4a5e4864feea098062f7660252b3e42cc63d7845
christopher paladino <paladin.birth@gmail.com>
2021-06-26 13:02:00 -0300

    created wabanex app

---
+++ wabanex/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :wabanex,
+  ecto_repos: [Wabanex.Repo]
+
+# Configures the endpoint
+config :wabanex, WabanexWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "z8eXTTHRrG0HWq6QfiVLIGieGdj9c0wGVlXJ2rOWf2bJUpKnau5edtMcvV8Vtdx7",
+  render_errors: [view: WabanexWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Wabanex.PubSub,
+  live_view: [signing_salt: "+of4oZKz"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
